5d5b170e84184dc5a01393b7b744017de5f29d07
Anushka Agarwal <agarwalanushka4@gmail.com>
2025-07-07 15:50:13 +0530

    feat: setting up the server

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"ai-smart-audit/config"
+	"context"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Server is running...")
+	_ = context.Background()
+	config := config.NewAppConfig()
+	//Todo: Pass all the functions that need to validate the config, like db, cache etc
+	config.Validate()
 }
